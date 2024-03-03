4f40c1658f9faee727b896602b322e78c773d9b2
Your Name <you@example.com>
2024-03-03 15:53:54 +0200

    finished TextReader class

--- all_file_readers/text_reader.py
+++ all_file_readers/text_reader.py
@@ -1,8 +1,20 @@
+"""
+    Purpose: Implements TextReader class
+    Author: Annie_nev
+    Created:3.3.2024
+"""
 from file_reader import FileReader
 
 
 class TextReader(FileReader):
+    """
+     Implements TextReader functions
+    """
     def read(self):
+        """
+        reads text files
+        :return: text file content
+        """
         file_content = open(self.file_path, "r")
         return file_content.read()
 
